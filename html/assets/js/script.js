97a1093b5f9487d2f572bd431c11860bd8c3a7db
MaDHouSe <madhouse1979@gmail.com>
2024-04-05 10:15:39 +0200

    Update script.js

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,6 +1,7 @@
 var timeleft;
 var timer;
 var message;
+var restartBy;
 
 function fancyTimeFormat(duration) {
     const hrs = ~~(duration / 3600);
@@ -8,12 +9,12 @@ function fancyTimeFormat(duration) {
     const secs = ~~duration % 60;
     let ret = "";
     if (hrs > 0) {
-        ret += "<span style='color:red'>" + hrs + "</span> hours" + (mins < 10 ? "<span style='color:red'>0</span>" : "");
+        ret += "<span style='color:yellow'>" + hrs + "</span> hours" + (mins < 10 ? "<span style='color:yellow'>0</span>" : "");
     }
     if (mins > 0) {
-        ret += "<span style='color:red'>" + mins + "</span> mins " + (secs < 10 ? "<span style='color:red'>0</span>" : "");
+        ret += "<span style='color:yellow'>" + mins + "</span> mins " + (secs < 10 ? "<span style='color:yellow'>0</span>" : "");
     }
-    ret += "<span style='color:red'>" + secs + "</span> secs";
+    ret += "<span style='color:yellow'>" + secs + "</span> secs";
     return ret;
 }
 
@@ -24,13 +25,19 @@ function StopTimer() {
 function StartTimer() {
     timer = setInterval(function () {
         timeleft = timeleft - 1;
+        document.getElementById("reason").innerHTML = restartBy
         document.getElementById("count").innerHTML = "<span style='color:white'>" + message + "</span> " + fancyTimeFormat(timeleft);
         if (timeleft <= 0) { clearInterval(timer); } 
     }, 1000);
+
+    if (timer) {
+        $(".container").css("display", "block");
+    }
 }
 
 window.addEventListener('message', function (event) {
     if (event.data.open) {
+        restartBy = event.data.restartBy;
         message = event.data.message;
         timeleft = event.data.secs;
         StartTimer();
