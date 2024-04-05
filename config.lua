887579e906f56081c509c0f72f950ce105bf5859
MaDHouSe <madhouse1979@gmail.com>
2024-04-05 10:18:08 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,7 +5,8 @@ Locales = {}
 Config = {}
 Config.Locale = "en"
 
-Config.ServerName = "RP Server"
+Config.ServerName = "--[[ RP Server name here ]]--"
+Config.RestartCommand = "restart-server"
 
 Config.Settings = {
     Weather = {
@@ -16,37 +17,37 @@ Config.Settings = {
     Time = {
         Enable = false,
         Hour = 00,
-        Min = 00,
+        Min = 00
     },
     Sound = {
         Enable = true,
         File = 'Alert.ogg',
-        Volume = 5.0,
+        Volume = 5.0
     },
     Falldown = {
         Enable = true,
-        Timer = math.random(15000, 30000),
+        Timer = math.random(15000, 30000)
     },
     Zombies = {
         Enable = true,
         UseBlood = true,
-        UseDamage = false,
-    },
+        UseDamage = false
+    }
 }
 
 function CreateString(str, ...)
-	if Locales[Config.Locale] ~= nil then
-		if Locales[Config.Locale][str] ~= nil then
-			return string.format(Locales[Config.Locale][str], ...)
-		else
-			return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exist'
-		end
-	else
-		return 'Locale [' .. Config.Locale .. '] does not exist'
-	end
+    if Locales[Config.Locale] ~= nil then
+        if Locales[Config.Locale][str] ~= nil then
+            return string.format(Locales[Config.Locale][str], ...)
+        else
+            return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exist'
+        end
+    else
+        return 'Locale [' .. Config.Locale .. '] does not exist'
+    end
 end
 
 -- Translate string first char uppercase
 function String(str, ...)
-	return tostring(CreateString(str, ...):gsub("^%l", string.upper))
+    return tostring(CreateString(str, ...):gsub("^%l", string.upper))
 end
